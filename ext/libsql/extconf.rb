9446e5387d0865b8213daa189ac2381ba2180e6d
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-07-03 12:23:16 +0100

    ci: bump
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- ext/libsql/extconf.rb
+++ ext/libsql/extconf.rb
@@ -6,12 +6,12 @@ have_library("c")
 have_header("stdio.h")
 have_header("bindings.h")
 
+# TODO: replace this with something that can actually build properly
 dir = File.expand_path(File.join(__dir__, "../../target/release"))
-libsql = "libsql_ruby"
+rust_binary = File.join(dir, "liblibsql_ruby.a")
 
-rust_binary = File.join(dir, "lib#{libsql}.a")
 abort "libsql bin not found: #{rust_binary}" unless File.exist?(rust_binary)
 
-$LDFLAGS << " -L#{dir} -l#{libsql}" # rubocop:disable Style/GlobalVars
+$LDFLAGS << " -L#{dir} -llibsql_ruby" # rubocop:disable Style/GlobalVars
 
 create_makefile "libsql/libsql"
