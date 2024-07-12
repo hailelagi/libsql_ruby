fbda82312e79c118c6cd3e563699b4db60a5b356
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-07-12 21:41:38 +0100

    sanity check bindgen
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- ext/libsql/libsql.c
+++ ext/libsql/libsql.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
 #include <ruby.h>
 #include "bindings.h"
+
+#ifndef LIBSQL_INT
+#error LibSQL bindings are not generated correctly or there was a problem linking
+#endif
 
 
 void Init_libsql(void)
 {    VALUE libSQL;
+    libSQL = rb_define_module("LibSQL");
 
     libSQL = rb_const_get(rb_cObject, rb_intern("LibSQL"));
 
